0a965afcae6dcd6a2e8572b8060eb4be009ea8bc
Dennis Vinterfjärd <dennis@vinterfjard.com>
2025-08-16 00:31:24 +0200

    Moving from my custom neovim setup to lazyvim instead

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,2 @@
---Setup lazy.nvim
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Load the rest of the configuration
-require("core")
-require("core.plugin_config")
-require("impl.tsconfig")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/lua/core/plugin_config/maximize_config.lua
+++
@@ -1,2 +0,0 @@
-require("maximize").setup()
-vim.keymap.set("n", "<Leader>z", "<Cmd>lua require('maximize').toggle()<CR>")
